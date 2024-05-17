05332b6060f241ed6e5a4f10927e30af6ff345fc
EC2 Default User <ec2-user@ip-172-31-92-153.ec2.internal>
2024-05-17 06:45:28 +0000

    added comments, probably last push

--- my_codebuild/my_codebuild_stack.py
+++ my_codebuild/my_codebuild_stack.py
@@ -14,37 +14,42 @@ class MyCodebuildStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # creating the CodeCommit repository. also does a directory input to find
+        # the java-project.zip file
         repo = CodeCommit.Repository(self, "Repository",
             repository_name= "java-project",
             description="The repository",
             code=CodeCommit.Code.from_zip_file("java-project.zip", "main"))
         
-        
+        # creating a role with policy for the CodeBuild project
         app_build_role = iam.Role(self,"AppBuildRole",assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"))
         # app_build_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("CodeBuildAccess"))
         
-        policy_document = iam.PolicyDocument(statements=[iam.PolicyStatement(
-            resources=["*"],
-            actions=["logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents"]
-            )])
         
-        build_log_policy = iam.Policy(self,"BuildLogPolicy",
-            document=policy_document)
+        # creating a policy document for the BuildLogPolicy
+        # policy_document = iam.PolicyDocument(statements=[iam.PolicyStatement(
+        #     resources=["*"],
+        #     actions=["logs:CreateLogGroup",
+        #             "logs:CreateLogStream",
+        #             "logs:PutLogEvents"]
+        #     )])
+        # build_log_policy = iam.Policy(self,"BuildLogPolicy",
+        #     document=policy_document)
         
         
+        # creating an S3 bucket for the CodeBuild project
+        artifact_bucket = s3.Bucket(self,"ArtifactBucket")
+
+        # creating a policy statement to add to the artifact bucket
         bucket_policy_statement = iam.PolicyStatement(
             resources=["*"],
             actions=["s3:PutObject"],
             effect=iam.Effect.DENY)
-        
         bucket_policy_statement.add_any_principal()
-            
-        artifact_bucket = s3.Bucket(self,"ArtifactBucket")
         artifact_bucket.add_to_resource_policy(bucket_policy_statement)
         
-
+        # creating a CodeBuild project. inserts the s3 bucket, iam role, and
+        # CodeCommit repository
         project = CodeBuild.Project(self,"AppBuildProject",
             artifacts=CodeBuild.Artifacts.s3(
                 bucket=artifact_bucket,
